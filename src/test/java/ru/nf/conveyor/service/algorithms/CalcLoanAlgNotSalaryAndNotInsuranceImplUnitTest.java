b4adf11a873d9dbb78bf272a15138184f089ec58
d.pinyugina <d.pinyugina@gmail.com>
2022-07-29 11:21:08 +0400

    Тест для кредитного предложения по шаблону 1 - isSalaryClient = false, isInsuranceEnabled = false

--- src/test/java/ru/nf/conveyor/service/algorithms/CalcLoanAlgNotSalaryAndNotInsuranceImplUnitTest.java
+++ src/test/java/ru/nf/conveyor/service/algorithms/CalcLoanAlgNotSalaryAndNotInsuranceImplUnitTest.java
@@ -7,13 +7,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.nf.conveyor.configuration.properties.ConveyorProperties;
 import ru.nf.conveyor.model.LoanApplicationRequestDTO;
 import ru.nf.conveyor.model.LoanOfferDTO;
 
-@ExtendWith(SpringExtension.class)
+@SpringBootTest
 @ContextConfiguration(classes = CalcLoanTestConfiguration.class)
 public class CalcLoanAlgNotSalaryAndNotInsuranceImplUnitTest {
 
@@ -23,16 +24,22 @@ public class CalcLoanAlgNotSalaryAndNotInsuranceImplUnitTest {
 	@Test
 	@DisplayName("Проверка кредитного предложения 1: isSalaryClient = false, isInsuranceEnabled = false")
 	void CalcLoanAlgNotSalaryAndNotInsuranceSuccess() {
-
 		//GIVEN
 		LoanApplicationRequestDTO requestDTO = new LoanApplicationRequestDTO();
 		requestDTO.setTerm(7);
+		requestDTO.setAmount(15000.00);
 
 		//WHEN
 		LoanOfferDTO loanOffer = calcLoanAlgNotSalaryAndNotInsurance.getLoanOffer(requestDTO);
 
 		//THEN
-		Assertions.assertEquals(7,loanOffer.getTerm());
-
+		Assertions.assertNotNull(loanOffer);
+		Assertions.assertEquals(7, loanOffer.getTerm());
+		Assertions.assertEquals(2289.0, loanOffer.getMonthlyPayment());
+		Assertions.assertEquals(false,loanOffer.getIsSalaryClient());
+		Assertions.assertEquals(false,loanOffer.getIsInsuranceEnabled());
+		Assertions.assertEquals(15000,loanOffer.getRequestedAmount());
+		Assertions.assertEquals(32523.0,loanOffer.getTotalAmount());
+		Assertions.assertEquals(20,loanOffer.getRate());
 	}
 }
